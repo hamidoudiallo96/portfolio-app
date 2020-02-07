09369d4edd39032406731fceb6540b5c2fd62f85
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-02-06 20:42:39 -0500

    navbar complete

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "./NavBar.styles.scss";
 
 export default function NavBar() {
-	return <div></div>;
+	return (
+		<div className="nav-list">
+			<li>About</li>
+			<li>Projects</li>
+			<li>Contact</li>
+		</div>
+	);
 }
--- src/components/navbar/NavBarContainer.jsx
+++ src/components/navbar/NavBarContainer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import NavBar from "./NavBar";
+import navLogo from "../../assets/22b39ef1-996d-4498-9bf6-346a6c019ee1_200x200.png";
+import "./NavBar.styles.scss";
 
 export default function NavBarContainer() {
 	return (
 		<div>
-			<NavBar />
+			<ul className="nav-container">
+				<img src={navLogo} alt="logo" height="100px" />
+				<NavBar />
+			</ul>
 		</div>
 	);
 }
